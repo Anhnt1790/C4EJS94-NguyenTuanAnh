cee8e615ac4de03fe819b3ebb7958d23dce50938
unknown <anhnt1790@gmail.com>
2020-08-17 20:15:20 +0700

    Add index.html + index.js

---
+++ Section-2/Lesson/index.js
@@ -0,0 +1,29 @@
+// let x = 0;
+// while(x<3){
+//     console.log("i love you")
+//     x++;
+// }
+
+// for (let x = 0; x < 3; x++) {
+//     console.log("i love you")
+// }
+
+// let age = prompt("Nhap tuoi cua ban");
+// if (age < 10) {
+//     alert("Em con nho lam");
+// }
+// else if(age<18){
+//     alert("minh di xem phim");
+// }
+// else {
+//     alert("minh di an kem");
+// }
+
+
+let age = prompt("nhap tuoi cua ban");
+if(age<10){
+    alert(true);
+}
+else{
+    alert(false);
+}
