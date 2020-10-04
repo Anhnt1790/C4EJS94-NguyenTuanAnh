4c986a744df7c04642dc48e11a30b5386418becc
anhnt1790 <anhnt1790@gmail.com>
2020-10-04 12:02:58 +0700

    add index.html + index.js + style.css

---
+++ NguyenTuanAnhTestCI/A/index.js
@@ -0,0 +1,6 @@
+console.log("Cho n số nguyên liên tiếp từ 0 đến n-1 được biểu diễn trên một vòng tròn, khoảng cách giữa các số bằng nhau (và tất nhiên là 0 và số n-1 cũng ở cạnh nhau).Biết rằng n là số nguyên dương chẵn nằm trong khoảng từ 4 đến 20")
+console.log("Viết hàm findOppositeNumber(n,inputNumber) cho trước 2 tham số đầu vào là n và inputNumber tồn tại trong dãy.Tìm số đối diện với số inputNumber đó (0 <= inputNumber <= n-1)")
+
+function findOppositeNumber(n, inputNumber) {
+    if()
+}
---
+++ NguyenTuanAnhTestCI/B/index.js
@@ -0,0 +1,8 @@
+let srt1 = ("abc","123");
+let srt2 = ("abc","0123");
+let srt3 = ("abcd","123");
+
+let n1 = srt1.split();
+let n2 = srt2.split();
+let n3 = srt3.split();
+console.log(n1);
